910e6431d359065b436f9237d2717d8590eb27b0
Daniil Abdulov <abdulov.dany@yandex.ru>
2026-02-06 16:52:31 +0500

    1.1.13

--- dist/extensions/factory.js
+++ dist/extensions/factory.js
@@ -4,17 +4,16 @@ exports.createExtensions = void 0;
 const knex_1 = require("knex");
 async function createExtensions(config) {
     const extensions = {};
-    console.log(`createExtensions`, config);
+    console.log(`create DB Extension`, config.dbConnection);
     if (config.dbConnection) {
         extensions.pg = (0, knex_1.knex)(config.dbConnection);
-        try {
-            await extensions.pg.raw('SELECT 1');
-            console.log('✅ PostgreSQL connected successfully');
-        }
-        catch (error) {
-            console.error('❌ PostgreSQL connection failed:', error);
-            throw error;
-        }
+        // try {
+        //   await extensions.pg.raw('SELECT 1');
+        //   console.log('✅ PostgreSQL connected successfully');
+        // } catch (error) {
+        //   console.error('❌ PostgreSQL connection failed:', error);
+        //   throw error;
+        // }
     }
     return extensions;
 }
